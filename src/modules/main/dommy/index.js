11875a33ac6a599bb0eb4e850598837d9b64446f
Nicolai Mølgaard Christensen <nmchr7@gmail.com>
2017-05-19 11:16:47 +0200

    - android measure button

--- src/modules/main/dommy/index.js
+++ src/modules/main/dommy/index.js
@@ -3,7 +3,10 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import {
+  Platform,
+  View
+} from 'react-native'
 
 
 import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
@@ -19,14 +22,43 @@ import { connect } from 'react-redux'
 
 class Dommy extends Component {
 
-  static navigationOptions = {
+  /*static navigationOptions = {
     showLabel: false,
     tabBarIcon: (props) => {
       return (<View style={{ backgroundColor: Colors.vaavudBlue, position: 'absolute', bottom: -35, height: 80, width: 80, alignItems: 'center', borderRadius: 40, paddingTop: 20 }}>
         <VaavudIcon name="logo" size={30} color="white" />
       </View >)
     }
+  }*/
+
+  static navigationOptions = Platform.OS === 'ios' ?
+  {
+    showLabel: false,
+    tabBarIcon: (props) => {
+      return (
+      <View style={
+        {
+          backgroundColor: Colors.vaavudBlue,
+          position: 'absolute',
+          bottom: -35, height: 80,
+          width: 80, alignItems: 'center',
+          borderRadius: 40,
+          paddingTop: 20
+        }}>
+        <VaavudIcon name="logo" size={30} color="white" />
+      </View >)
+    }
   }
+  :
+  {
+    showLabel: false,
+    tabBarIcon: (props) => {
+      return (
+        <VaavudIcon name="logo" size={25} color={Colors.vaavudBlue} />
+      )
+    }
+  }
+
 
   componentDidMount = () => {
     this.props.goToMeasurement()
