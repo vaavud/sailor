910761f560cda02eb99a6b7f2d319001426da42b
Juan Muñoz <juan@vaavud.com>
2017-06-02 00:30:05 +0200

    Bridge updates

--- android/app/src/main/java/com/vaavud/sailing/bridges/VaavudBLE.java
+++ android/app/src/main/java/com/vaavud/sailing/bridges/VaavudBLE.java
@@ -130,27 +130,51 @@ public class VaavudBLE extends ReactContextBaseJavaModule implements VaavudDataL
         aegirSDK.onStopSDK();
         mdata = aegirSDK.stopSession();
 
-        ArrayList<WritableMap> simplifiedLocation = new ArrayList<>();
-        ArrayList<WritableMap> simplifiedSpeed = new ArrayList<>();
-        ArrayList<WritableMap> simplifiedDirection = new ArrayList<>();
 
-        LatLng[] coords = mdata.getLocationsArray(); // the array of your "original" points
+        LatLng[] cords = mdata.getLocationsArray(); // the array of your "original" points
         TrueSpeedEvent[] speeds = mdata.getTrueWindSpeedsArray();
         TrueDirectionEvent[] directions = mdata.getTrueWindDirectionsArray();
+
 
         Simplify<LatLng> simplifyLatLng = new Simplify<LatLng>(new LatLng[0], latLngPointExtractor);
         Simplify<TrueSpeedEvent> simplifyTrueSpeed = new Simplify<TrueSpeedEvent>(new TrueSpeedEvent[0], speedPointExtractor);
         Simplify<TrueDirectionEvent> simplifyTrueDirection = new Simplify<TrueDirectionEvent>(new TrueDirectionEvent[0], directionPointExtractor);
 
-        LatLng[] simplifiedLatLng = simplifyLatLng.simplify(coords, 0.0001f, false);
+        LatLng[] simplifiedLatLng = simplifyLatLng.simplify(cords, 0.0001f, false);
         TrueSpeedEvent[] simplifiedTrueSpeed = simplifyTrueSpeed.simplify(speeds, 0.5f, false);
         TrueDirectionEvent[] simplifiedTrueDirection = simplifyTrueDirection.simplify(directions, 22.5f, false);
 
+        Log.d(TAG, "Sizes:" + simplifiedLatLng.length +" "+ simplifiedTrueDirection.length + " " + simplifiedTrueSpeed.length);
+
+        WritableArray simplifiedLatLngDict = Arguments.createArray();
+        for (int i=0;i<simplifiedLatLng.length;i++){
+            WritableMap map = Arguments.createMap();
+            map.putDouble("lat",simplifiedLatLng[i].getLatitude());
+            map.putDouble("lon",simplifiedLatLng[i].getLongitude());
+            simplifiedLatLngDict.pushMap(map);
+        }
+        WritableArray simplifiedTrueSpeedDict = Arguments.createArray();
+        for (int i=0;i<simplifiedTrueSpeed.length;i++){
+            WritableMap map = Arguments.createMap();
+            map.putDouble("timestamp",simplifiedTrueSpeed[i].getTime());
+            map.putDouble("windSpeed",simplifiedTrueSpeed[i].getTrueSpeed());
+            simplifiedTrueSpeedDict.pushMap(map);
+        }
+        WritableArray simplifiedTrueDirectionDict = Arguments.createArray();
+        for (int i=0;i<simplifiedTrueDirection.length;i++){
+            WritableMap map = Arguments.createMap();
+            map.putDouble("timestamp",simplifiedTrueDirection[i].getTime());
+            map.putDouble("windDirection",simplifiedTrueDirection[i].getTrueDirection());
+            simplifiedTrueDirectionDict.pushMap(map);
+        }
+
+        //Log.d(TAG,"Arguments "+ Arguments.fromJavaArgs(simplifiedTrueDirectionDict));
         HashMap<String, Object> data = new HashMap<>();
-        data.put("points",mdata.getMeasurementPointsArray());
-        data.put("locations",simplifiedLatLng);
-        data.put("speeds",simplifiedTrueSpeed);
-        data.put("directions",simplifiedTrueDirection);
+        data.put("measurementPoints",mdata.getMeasurementPoints());
+        data.put("locations",simplifiedLatLngDict);
+        data.put("speeds",simplifiedTrueSpeedDict);
+        data.put("directions",simplifiedTrueDirectionDict);
+        data.put("session",mdata.getSession());
 
         emitData("onFinalData",hashMapToWritableMap(data));
 //        callback.resolve(null);
@@ -204,13 +228,16 @@ public class VaavudBLE extends ReactContextBaseJavaModule implements VaavudDataL
             Iterator it = hash.keySet().iterator();
             while (it.hasNext()){
                 String next = (String) it.next();
-                Log.d(TAG,next + " " + hash.get(next).getClass().getName());
+                //Log.d(TAG,next + " " + hash.get(next).getClass().getName());
                 switch (hash.get(next).getClass().getName()){
                     case "java.lang.Boolean":
                         map.putBoolean(next,(boolean)hash.get(next));
                         break;
                     case "java.lang.Integer":
                         map.putInt(next,(int)hash.get(next));
+                        break;
+                    case "java.lang.Long":
+                        map.putDouble(next,((Long)hash.get(next)).doubleValue());
                         break;
                     case "java.lang.Double":
                         //Log.d(TAG,next + " "+ (double)hash.get(next));
@@ -220,6 +247,7 @@ public class VaavudBLE extends ReactContextBaseJavaModule implements VaavudDataL
                         map.putString(next,(String)hash.get(next));
                         break;
                     case "java.lang.Object[]":
+                        Log.d(TAG,"Next_java.lang.Object[] " + next);
                         map.putArray(next,Arguments.fromJavaArgs((Object[]) hash.get(next)));
                         break;
                     case "java.util.ArrayList":
@@ -231,6 +259,12 @@ public class VaavudBLE extends ReactContextBaseJavaModule implements VaavudDataL
                     case "com.facebook.react.bridge.WritableNativeMap":
                         map.putMap(next,(WritableMap) hash.get(next));
                         break;
+                    case "com.facebook.react.bridge.WritableNativeArray":
+                        map.putArray(next,(WritableArray) hash.get(next));
+                        break;
+                    default:
+                        Log.d(TAG,"Default: " + next);
+                        map.putArray(next,Arguments.fromJavaArgs((Object[]) hash.get(next)));
                 }
             }
         }
@@ -241,31 +275,31 @@ public class VaavudBLE extends ReactContextBaseJavaModule implements VaavudDataL
 
         WritableNativeArray arguments = new WritableNativeArray();
         for (int i = 0; i < array.size(); i++) {
-          Object argument = array.get(i);
-          if (argument == null) {
-            arguments.pushNull();
-            continue;
-          }
-          Class argumentClass = argument.getClass();
-          if (argumentClass == Boolean.class) {
+            Object argument = array.get(i);
+            if (argument == null) {
+                arguments.pushNull();
+                continue;
+            }
+            Class argumentClass = argument.getClass();
+            if (argumentClass == Boolean.class) {
                 arguments.pushBoolean(((Boolean) argument).booleanValue());
-          } else if (argumentClass == Integer.class) {
+            } else if (argumentClass == Integer.class) {
                 arguments.pushDouble(((Integer) argument).doubleValue());
-          } else if (argumentClass == Double.class) {
+            } else if (argumentClass == Double.class) {
                 arguments.pushDouble(((Double) argument).doubleValue());
-          } else if (argumentClass == Float.class) {
+            } else if (argumentClass == Float.class) {
                 arguments.pushDouble(((Float) argument).doubleValue());
-          } else if (argumentClass == String.class) {
+            } else if (argumentClass == String.class) {
                 arguments.pushString(argument.toString());
-          } else if (argumentClass == ArrayList.class) {
+            } else if (argumentClass == ArrayList.class) {
                 arguments.pushArray(arrayListToWritableArray((ArrayList<Object>) argument));
-          } else if (argumentClass == HashMap.class){
-                arguments.pushMap(hashMapToWritableMap((HashMap<String,Object>) argument));
-          } else if (argumentClass == WritableNativeMap.class) {
+            } else if (argumentClass == HashMap.class) {
+                arguments.pushMap(hashMapToWritableMap((HashMap<String, Object>) argument));
+            } else if (argumentClass == WritableNativeMap.class) {
                 arguments.pushMap((WritableNativeMap) argument);
-          } else if (argumentClass == WritableNativeArray.class) {
+            } else if (argumentClass == WritableNativeArray.class) {
                 arguments.pushArray((WritableNativeArray) argument);
-          } else {
+            } else {
             throw new RuntimeException("Cannot convert argument of type " + argumentClass);
           }
         }
